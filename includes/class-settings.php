29943bc87ed1e55b7d95b15d3b90b7db7cc1be34
NhanSamLe <ledaonhansam001@gmail.com>
2025-12-11 09:02:32 +0700

    refactor(setting): edit prefix

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -1,13 +1,13 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
-class SS_Settings {
+class Settings {
 
     public static function get_token() {
-        return get_option('ss_access_token', '');
+        return get_option('access_token', '');
     }
 
     public static function save_token($token) {
-        update_option('ss_access_token', sanitize_text_field($token));
+        update_option('access_token', sanitize_text_field($token));
     }
 }
