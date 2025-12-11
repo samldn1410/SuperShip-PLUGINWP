08af06c40b27e224b6426b7d205146cd932ea47d
NhanSamLe <ledaonhansam001@gmail.com>
2025-12-11 09:01:35 +0700

    feat(menu): add warehouse and webhook menu

--- includes/class-admin-menu.php
+++ includes/class-admin-menu.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
-class SS_Admin_Menu {
+class Admin_Menu {
 
     public static function init() {
         add_action('admin_menu', [__CLASS__, 'register']);
@@ -9,20 +9,59 @@ class SS_Admin_Menu {
 
     public static function register() {
         add_menu_page(
-            'SuperShip',
-            'SuperShip',
-            'manage_options',
-            'ss-settings',
-            '',
-            'dashicons-location-alt'
+            'SuperShip', // Page Title
+            'SuperShip', // Menu Title
+            'manage_options', // Quyền truy cập, chỉ admin mới được thấy
+            'supership-settings', // Menu Slug
+            '', // hàm hiển thị nội dung trnag
+            'dashicons-location-alt' 
         );
 
-        add_submenu_page('ss-settings', 'Cài đặt', 'Cài đặt', 'manage_options', 'ss-settings', function () {
-            include SS_DIR . 'views/settings.php';
+        add_submenu_page('supership-settings',  // parent slug
+        'Cài đặt', //Page Title
+        'Cài đặt', // Menu Title
+        'manage_options',
+         'supership-settings',  
+         function () {
+            include DIR . 'views/settings.php'; // hàm hiển thị nội dung trang
+        });
+        add_submenu_page(
+            'supership-settings',
+            'Cấu hình Webhook',
+            'Webhook',
+            'manage_options',
+            'supership-webhook',
+             function () {
+                include DIR . 'views/webhook-settings.php';
+            }
+        );
+        add_submenu_page(
+            'supership-settings',
+            'Webhook Logs',
+            'Webhook Logs',
+            'manage_options',
+            'supership-webhook-logs',
+            function () {
+                include DIR . 'views/webhook-logs.php';
+            }
+        );
+
+        add_submenu_page('supership-settings', 'Tạo đơn', 'Tạo đơn', 'manage_options', 'supership-create', function () {
+            include DIR . 'views/create-order.php';
         });
 
-        add_submenu_page('ss-settings', 'Tạo đơn', 'Tạo đơn', 'manage_options', 'ss-create', function () {
-            include SS_DIR . 'views/create-order.php';
-        });
+      
+        add_submenu_page(
+            'supership-settings',
+            'Kho hàng',
+            'Kho hàng',
+            'manage_options',
+            'supership-warehouses',
+            [__CLASS__, 'view_list']
+        );
+
     }
+         public static function view_list() {
+             include DIR . 'views/warehouse.php';
+        }    
 }
